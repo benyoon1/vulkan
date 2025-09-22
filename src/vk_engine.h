d1a5d90758388f581533cf181ac97d13dd923f3d
Bin <bin.yoon@outlook.com>
2025-09-22 00:03:41 -0400

    add clang-format

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -5,47 +5,48 @@
 
 #include <vk_types.h>
 
-class VulkanEngine {
+class VulkanEngine
+{
 public:
-  bool _isInitialized{false};
-  int _frameNumber{0};
-  bool stop_rendering{false};
-  VkExtent2D _windowExtent{1700, 900};
+    bool _isInitialized{false};
+    int _frameNumber{0};
+    bool stop_rendering{false};
+    VkExtent2D _windowExtent{1700, 900};
 
-  VkInstance _instance;                      // Vulkan library handle
-  VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
-  VkPhysicalDevice _chosenGPU;               // GPU chosen as the default device
-  VkDevice _device;                          // Vulkan device for commands
-  VkSurfaceKHR _surface;                     // Vulkan window surface
+    VkInstance _instance;                      // Vulkan library handle
+    VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+    VkPhysicalDevice _chosenGPU;               // GPU chosen as the default device
+    VkDevice _device;                          // Vulkan device for commands
+    VkSurfaceKHR _surface;                     // Vulkan window surface
 
-  struct SDL_Window *_window{nullptr};
+    struct SDL_Window* _window{nullptr};
 
-  //> swap_init
-  VkSwapchainKHR _swapchain;
-  VkFormat _swapchainImageFormat;
+    //> swap_init
+    VkSwapchainKHR _swapchain;
+    VkFormat _swapchainImageFormat;
 
-  std::vector<VkImage> _swapchainImages;
-  std::vector<VkImageView> _swapchainImageViews;
-  VkExtent2D _swapchainExtent;
-  //< swap_init
+    std::vector<VkImage> _swapchainImages;
+    std::vector<VkImageView> _swapchainImageViews;
+    VkExtent2D _swapchainExtent;
+    //< swap_init
 
-  // initializes everything in the engine
-  void init();
+    // initializes everything in the engine
+    void init();
 
-  // shuts down the engine
-  void cleanup();
+    // shuts down the engine
+    void cleanup();
 
-  // draw loop
-  void draw();
+    // draw loop
+    void draw();
 
-  // run main loop
-  void run();
+    // run main loop
+    void run();
 
 private:
-  void init_vulkan();
-  void init_swapchain();
-  void init_commands();
-  void init_sync_structures();
-  void create_swapchain(uint32_t width, uint32_t height);
-  void destroy_swapchain();
+    void init_vulkan();
+    void init_swapchain();
+    void init_commands();
+    void init_sync_structures();
+    void create_swapchain(uint32_t width, uint32_t height);
+    void destroy_swapchain();
 };
