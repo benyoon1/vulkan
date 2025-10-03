880697e71adbf782653b053518048664974dbdd2
Bin <bin.yoon@outlook.com>
2025-10-03 00:42:37 -0400

    descriptor sets/allocator, imgui

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -3,18 +3,61 @@
 
 #pragma once
 
+#include "vk_descriptors.h"
+#include "vk_mem_alloc.h"
+#include <deque>
+#include <functional>
+#include <vector>
 #include <vk_types.h>
+
+struct DeletionQueue
+{
+    std::deque<std::function<void()>> deletors;
+
+    void push_function(std::function<void()>&& function) { deletors.push_back(function); }
+
+    void flush()
+    {
+        // reverse iterate the deletion queue to execute all the functions
+        for (auto it = deletors.rbegin(); it != deletors.rend(); it++)
+        {
+            (*it)(); // call functors
+        }
+
+        deletors.clear();
+    }
+};
 
 struct FrameData
 {
-    VkSemaphore _swapchainSemaphore; // signaled by acquire; per-frame is fine
+    VkSemaphore _swapchainSemaphore;
     VkFence _renderFence;
 
     VkCommandPool _commandPool;
     VkCommandBuffer _mainCommandBuffer;
+
+    DeletionQueue _deletionQueue;
 };
 
 constexpr unsigned int FRAME_OVERLAP = 2;
+
+struct ComputePushConstants
+{
+    glm::vec4 data1;
+    glm::vec4 data2;
+    glm::vec4 data3;
+    glm::vec4 data4;
+};
+
+struct ComputeEffect
+{
+    const char* name;
+
+    VkPipeline pipeline;
+    VkPipelineLayout layout;
+
+    ComputePushConstants data;
+};
 
 class VulkanEngine
 {
@@ -26,34 +69,52 @@ public:
 
     struct SDL_Window* _window{nullptr};
 
-    //> inst_init
-    VkInstance _instance;                      // Vulkan library handle
-    VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
-    VkPhysicalDevice _chosenGPU;               // GPU chosen as the default device
-    VkDevice _device;                          // Vulkan device for commands
-    VkSurfaceKHR _surface;                     // Vulkan window surface
-                                               //< inst_init
+    VkInstance _instance;
+    VkDebugUtilsMessengerEXT _debug_messenger;
+    VkPhysicalDevice _chosenGPU;
+    VkDevice _device;
 
-    //> queues
     FrameData _frames[FRAME_OVERLAP];
 
     FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
 
     VkQueue _graphicsQueue;
     uint32_t _graphicsQueueFamily;
-    //< queues
 
-    //> swap_init
+    VkSurfaceKHR _surface;
     VkSwapchainKHR _swapchain;
     VkFormat _swapchainImageFormat;
+    VkExtent2D _swapchainExtent;
+    VkExtent2D _drawExtent;
 
+    DescriptorAllocator globalDescriptorAllocator;
+
+    VkPipeline _gradientPipeline;
+    VkPipelineLayout _gradientPipelineLayout;
+
+    std::vector<VkFramebuffer> _framebuffers;
     std::vector<VkImage> _swapchainImages;
     std::vector<VkImageView> _swapchainImageViews;
-    VkExtent2D _swapchainExtent;
     // One render-finished semaphore per swapchain image to avoid reuse while present is in-flight
     std::vector<VkSemaphore> _imageRenderSemaphores;
-    //< swap_init
 
+    VkDescriptorSet _drawImageDescriptors;
+    VkDescriptorSetLayout _drawImageDescriptorLayout;
+
+    DeletionQueue _mainDeletionQueue;
+
+    VmaAllocator _allocator; // vma lib allocator
+    // immediate submit structures
+    VkFence _immFence;
+    VkCommandBuffer _immCommandBuffer;
+    VkCommandPool _immCommandPool;
+
+    // draw resources
+    AllocatedImage _drawImage;
+
+    std::vector<ComputeEffect> backgroundEffects;
+
+    int currentBackgroundEffect{0};
     // initializes everything in the engine
     void init();
 
@@ -63,14 +124,30 @@ public:
     // draw loop
     void draw();
 
+    void draw_background(VkCommandBuffer cmd);
+    void draw_imgui(VkCommandBuffer cmd, VkImageView targetImageView);
+
     // run main loop
     void run();
 
+    void immediate_submit(std::function<void(VkCommandBuffer cmd)>&& function);
+
 private:
+    void rebuild_swapchain();
+
     void init_vulkan();
+
     void init_swapchain();
-    void init_commands();
-    void init_sync_structures();
     void create_swapchain(uint32_t width, uint32_t height);
     void destroy_swapchain();
+
+    void init_commands();
+
+    void init_pipelines();
+
+    void init_descriptors();
+
+    void init_sync_structures();
+
+    void init_imgui();
 };
