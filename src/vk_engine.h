b31d3d12a0d005a1f96ac90a611160659f1f5c6a
Bin <bin.yoon@outlook.com>
2025-10-12 23:23:29 -0400

    change camera position, viewport size and formatting

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -177,7 +177,7 @@ public:
     bool _isInitialized{false};
     int _frameNumber{0};
 
-    VkExtent2D _windowExtent{1700, 900};
+    VkExtent2D _windowExtent{1920, 1080};
 
     struct SDL_Window* _window{nullptr};
 
